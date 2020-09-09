98b56781aafb909be681d0aaa14e80d98b9bc765
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-09 10:20:22 -0400

    AgentController → CustomerController

--- javaorders/src/main/java/tech/gebel/javaorders/controllers/CustomerController.java
+++ javaorders/src/main/java/tech/gebel/javaorders/controllers/CustomerController.java
@@ -1,16 +1,20 @@
 package tech.gebel.javaorders.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.gebel.javaorders.repositories.AgentsRepository;
 
 @RestController
-@RequestMapping("/agents")
 public class AgentController {
   private final AgentsRepository agentsRepository;
 
   public AgentController(AgentsRepository agentsRepository) {
     this.agentsRepository = agentsRepository;
+  }
+
+  @GetMapping("/customers/orders")
+  public ResponseEntity<?> listCustomersWithOrders() {
+    final list
   }
 }
