c544e0b0c8fc5f5a4ac9cb0d43fe2fc8e96fbfe3
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-09 11:53:58 -0400

    Add RequestMapping to CustomerController

--- javaorders/src/main/java/tech/gebel/javaorders/controllers/CustomerController.java
+++ javaorders/src/main/java/tech/gebel/javaorders/controllers/CustomerController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.gebel.javaorders.models.Customer;
 import tech.gebel.javaorders.repositories.AgentsRepository;
 import tech.gebel.javaorders.services.CustomerService;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
   private final CustomerService customerService;
 
@@ -17,8 +19,8 @@ public class CustomerController {
     this.customerService = customerService;
   }
 
-  @GetMapping("/customers/orders")
-  public ResponseEntity<?> listCustomersWithOrders() {
+  @GetMapping("/orders")
+  private ResponseEntity<?> listCustomersWithOrders() {
     final List<Customer> list = customerService.findAllCustomers();
     return new ResponseEntity<>(list, HttpStatus.OK);
   }
