39e42d3c8a9eebde5e9950fdbce72a44878970d1
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-11 00:37:10 -0400

    Create long, double, and Object versions optionallyReplace static method

--- javaorders/src/main/java/tech/gebel/javaorders/Utility.java
+++ javaorders/src/main/java/tech/gebel/javaorders/Utility.java
@@ -3,4 +3,16 @@ package tech.gebel.javaorders;
 public final class Utility {
 
   private Utility() {}
+
+  public static Object optionallyReplace(Object receiver, Object provider) {
+    if (provider == null) return receiver; else return provider;
+  }
+
+  public static long optionallyReplace(long receiver, long provider) {
+    if (provider == 0) return receiver; else return provider;
+  }
+
+  public static double optionallyReplace(double receiver, double provider) {
+    if (provider == 0.0d) return receiver; else return provider;
+  }
 }
